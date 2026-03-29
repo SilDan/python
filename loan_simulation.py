5ff10e1306225eac2a19596ff1a4c1663f6eb1ef
sildan <silvan.rincke@gmx.de>
2026-03-29 15:56:15 +0200

    loan_simulation-3: Create a dataclass instance

--- loan_simulation.py
+++ loan_simulation.py
@@ -1,6 +1,28 @@
-def main():
-	print("loan simulation starting...")
+from dataclasses import dataclass
 
+@dataclass
+class MonthEntry:
+    month_index: int
+    label: str
+    balance_start: float
+    interest: float
+    principal: float
+    payment: float
+    balance_end: float
+
+
+def main():
+    entry = MonthEntry(
+        month_index=0,
+        label="April 2026",
+        balance_start=14500.0,
+        interest=50.0,
+        principal=100.0,
+        payment=150.0,
+        balance_end=14400.0,
+)
+
+    print(entry)
 
 if __name__ == "__main__":
-	main()
+    main()
